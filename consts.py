3b4b8335d1152725726115196380fd2bc19790e3
Alyona1619 <108504553+Alyona1619@users.noreply.github.com>
2023-05-18 01:00:53 +0800

    Create consts.py

---
+++ consts.py
@@ -0,0 +1,13 @@
+from messages import *
+
+
+BOTTOKEN = '5136913496:AAFVF_8Ua_bXVTGtC3N6XuMI8YbTZ_UOLCo'
+
+user_data = {}
+user_language = {}
+
+all_topics = list(messages['Русский'].keys()) + list(messages['English'].keys())
+
+N_TOKEN = 'secret_1RiuGReKtVd1FSDEqtOPLgf2FSHvJGvXmAOBwKCh710'
+DB_ID = 'a6fc9f7f128d4b33a37916eda8f78269'
+print(all_topics)
