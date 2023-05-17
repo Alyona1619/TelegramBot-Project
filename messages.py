3ac816f0f00b6ccb094a144efa1b5527f7798ea5
Alyona1619 <108504553+Alyona1619@users.noreply.github.com>
2023-05-17 17:07:33 +0800

    Create messages.py

---
+++ messages.py
@@ -0,0 +1,46 @@
+messages = {
+    'Русский': {
+        'Подготовка к процедуре': {
+            'text': 'Текст о подготовке к процедуре',
+            'image_url': 'url_подготовки_к_процедуре'
+        },
+        'Классический массаж': {
+            'text': 'Текст о классическом массаже',
+            'image_url': 'url_классического_массажа'
+        },
+        'Пластический массаж': {
+            'text': 'Текст о пластическом массаже',
+            'image_url': 'url_пластического_массажа'
+        },
+        'Лимфодренажный массаж': {
+            'text': 'Текст о лимфодренажном массаже',
+            'image_url': 'url_лимфодренажного_массажа'
+        },
+        'Польза массажа': {
+            'text': 'Текст о пользе массажа',
+            'image_url': 'url_пользы_массажа'
+        }
+    },
+    'English': {
+        'Preparation for the Procedure': {
+            'text': 'Text about preparation for the procedure',
+            'image_url': 'preparation_for_the_procedure_image_url'
+        },
+        'Swedish Massage': {
+            'text': 'Text about Swedish massage',
+            'image_url': 'swedish_massage_image_url'
+        },
+        'Deep Tissue Massage': {
+            'text': 'Text about deep tissue massage',
+            'image_url': 'deep_tissue_massage_image_url'
+        },
+        'Lymphatic Drainage Massage': {
+            'text': 'Text about lymphatic drainage massage',
+            'image_url': 'lymphatic_drainage_massage_image_url'
+        },
+        'Benefits of Massage': {
+            'text': 'Text about benefits of massage',
+            'image_url': 'benefits_of_massage_image_url'
+        }
+    }
+}
